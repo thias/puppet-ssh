bb8d317f5b0ba14737c7e95a69ac0572e68f4fb7
Matthias Saou <matthias@saou.eu>
2014-01-16 18:39:28 +0100

    Support UseDNS sshd_config option

--- manifests/sshd_config.pp
+++ manifests/sshd_config.pp
@@ -14,6 +14,7 @@ class ssh::sshd_config (
   $pubkeyauthentication   = undef,
   $passwordauthentication = undef,
   $x11forwarding          = undef,
+  $usedns                 = undef,
   $match                  = undef,
   $template_dir           = $::ssh::params::template_dir,
 ) inherits ::ssh::params {
